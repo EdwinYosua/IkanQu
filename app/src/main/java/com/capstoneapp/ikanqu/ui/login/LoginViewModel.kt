c8959d46579440ebab0a9389a08c3493cdf90377
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-06-16 11:49:26 +0800

    LoginViewModel
    adding new files LoginViewModel and modifying ViewModelFactory

---
+++ app/src/main/java/com/capstoneapp/ikanqu/ui/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.capstoneapp.ikanqu.ui.login
+
+import androidx.lifecycle.ViewModel
+import com.capstoneapp.ikanqu.repository.AppRepository
+
+class LoginViewModel(private val appRepo: AppRepository) : ViewModel() {
+}
