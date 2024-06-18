247f52826be8dc5cc52bc785c31c31a659ab6763
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-06-18 14:25:22 +0800

    Layout Fix
    change the splasscreen logo, fixing the keyboard overlap in RegisterActivity

--- app/src/main/java/com/capstoneapp/ikanqu/ui/main/MainActivity.kt
+++ app/src/main/java/com/capstoneapp/ikanqu/ui/main/MainActivity.kt
@@ -18,15 +18,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        supportActionBar?.hide()
         setContentView(binding.root)
 
         val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
         val mainViewModel: MainViewModel by viewModels { factory }
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
--- app/src/main/java/com/capstoneapp/ikanqu/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/capstoneapp/ikanqu/ui/register/RegisterActivity.kt
@@ -2,10 +2,15 @@ package com.capstoneapp.ikanqu.ui.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.capstoneapp.ikanqu.ViewModelFactory
 import com.capstoneapp.ikanqu.databinding.ActivityRegisterBinding
 import com.capstoneapp.ikanqu.network.ApiResult
@@ -19,7 +24,7 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(binding.root)
 
 
@@ -79,6 +84,29 @@ class RegisterActivity : AppCompatActivity() {
             }
         }
     }
+
+//    private fun hideKeyboard() {
+//        val imm =
+//            ContextCompat.getSystemService(this@RegisterActivity, InputMethodManager::class.java)
+//        val view = currentFocus ?: (this@RegisterActivity)
+//        imm?.hideSoftInputFromWindow(view.windowToken, 0)
+//    }
+//
+//    private fun setupUI(view: View) {
+//        if (view !is EditText) {
+//            view.setOnTouchListener { _, _ ->
+//                hideKeyboard()
+//                false
+//            }
+//        }
+//        if (view is ViewGroup) {
+//            for (i in 0 until view.childCount) {
+//                val innerView = view.getChildAt(i)
+//                setupUI(innerView)
+//            }
+//        }
+//    }
+
 
     private fun showToast(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
