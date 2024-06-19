6680b87dce77c5a72198e322b4425858b38b0d33
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-06-19 18:07:15 +0800

    HomeActivity.kt
    adding gallery pick media function

--- app/src/main/java/com/capstoneapp/ikanqu/ui/home/HomeActivity.kt
+++ app/src/main/java/com/capstoneapp/ikanqu/ui/home/HomeActivity.kt
@@ -1,25 +1,59 @@
 package com.capstoneapp.ikanqu.ui.home
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.capstoneapp.ikanqu.R
 import com.capstoneapp.ikanqu.ViewModelFactory
 import com.capstoneapp.ikanqu.databinding.ActivityHomeBinding
-import com.capstoneapp.ikanqu.ui.detail.DetailActivity
 import com.capstoneapp.ikanqu.ui.main.MainActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private var currentImg: Uri? = null
     private val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
     private val homeViewModel: HomeViewModel by viewModels { factory }
+
+    private val reqPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { granted ->
+        if (granted) {
+            Toast.makeText(this, "Permission Request Granted", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Permission Request Denied", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImg = uri
+            showImg()
+        } else {
+            Log.d("Photo Picker", "No Media Selected")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (!allPermissionGranted()) {
+            reqPermissionLauncher.launch(REQ_PERMISSION)
+        }
 
         val userName = intent.getStringExtra(EXTRA_NAME) ?: "404"
 
@@ -27,6 +61,8 @@ class HomeActivity : AppCompatActivity() {
 
             imageLabelTextView.text =
                 getString(R.string.hallo_text, userName.split(" ").firstOrNull() ?: userName)
+
+            galleryButton.setOnClickListener { startGallery() }
 
             analyzeButton.setOnClickListener {
 //                startActivity(Intent(this@HomeActivity, DetailActivity::class.java))
@@ -39,7 +75,24 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
+    private fun allPermissionGranted() =
+        ContextCompat.checkSelfPermission(
+            this, REQ_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun showImg() {
+        currentImg?.let {
+            Log.d("Img Uri", "Show Image : $it")
+            binding.previewImageView.setImageURI(it)
+        }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
     companion object {
+        private const val REQ_PERMISSION = Manifest.permission.CAMERA
         const val EXTRA_NAME = "extra_name"
     }
 }
