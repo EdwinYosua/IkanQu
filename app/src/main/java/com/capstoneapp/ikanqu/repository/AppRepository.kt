b8087478aa21a45b850ef3d92fb75e1253d53386
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-06-20 20:13:16 +0800

    Cloud ML
    modify Repository for accessing Model Api

--- app/src/main/java/com/capstoneapp/ikanqu/repository/AppRepository.kt
+++ app/src/main/java/com/capstoneapp/ikanqu/repository/AppRepository.kt
@@ -1,17 +1,44 @@
 package com.capstoneapp.ikanqu.repository
 
+import ModelResponse
 import com.capstoneapp.ikanqu.local.SettingPreference
 import com.capstoneapp.ikanqu.network.ApiResult
 import com.capstoneapp.ikanqu.network.ApiServices
+import com.capstoneapp.ikanqu.network.ModelApiServices
 import com.capstoneapp.ikanqu.network.response.LoginSuccess
 import com.capstoneapp.ikanqu.network.response.RegisterResponse
+import com.capstoneapp.ikanqu.utils.reduceFileImg
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 
 class AppRepository private constructor(
     private val apiServices: ApiServices,
+    private val modelApiServices: ModelApiServices,
     private val pref: SettingPreference
 ) {
+
+    fun analyzeImg(imgUri: File): Flow<ApiResult<ModelResponse>> = flow {
+
+        try {
+            emit(ApiResult.ApiLoading)
+
+            val img = imgUri.reduceFileImg()
+            val reqImgFile = img.asRequestBody("image/*".toMediaType())
+            val multipartBody = MultipartBody.Part.createFormData("file", img.name, reqImgFile)
+            val response = modelApiServices.predict(multipartBody)
+
+            //add error later
+            emit(ApiResult.ApiSuccess(response))
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(ApiResult.ApiError(e.message.toString()))
+        }
+    }
 
     fun register(
         name: String,
@@ -53,6 +80,7 @@ class AppRepository private constructor(
         }
     }
 
+
     fun getUserId() = pref.getUserId()
     fun getUserName() = pref.getUserName()
 
@@ -64,11 +92,13 @@ class AppRepository private constructor(
         private var instance: AppRepository? = null
         fun getInstance(
             apiServices: ApiServices,
+            modelApiServices: ModelApiServices,
             pref: SettingPreference
         ): AppRepository =
             instance ?: synchronized(this) {
                 instance ?: AppRepository(
                     apiServices,
+                    modelApiServices,
                     pref
                 ).also { instance = it }
             }
